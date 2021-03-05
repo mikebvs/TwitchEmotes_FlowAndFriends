0a0a846ccac9fd6e82a96c3739cb7920258ec5f0
Putro <laustsf@gmail.com>
2021-03-05 20:43:08 +0100

    [Emotes] Add more emotes, and clean up structuring for Wide and Large emotes

--- main.lua
+++ main.lua
@@ -1,32 +1,57 @@
 
 local solarisEmoticons = {
+    ["ayayaKMS"] = "ayayaKMS",
+    ["AzorCool"] = "AzorCool",
     ["blusherBucket"] = "blusherBucket",
+    ["catDisgust"] = "catDisgust",
+    ["DistortedWeird"] = "DistortedWeird",
     ["happyAwesome"] = "happyAwesome",
+    ["HappyPeepoFarmer"] = "HappyPeepoFarmer",
+    ["NootLikeThis"] = "NootLikeThis",
     ["peepoFlower"] = "peepoFlower",
     ["peepoHappyNuggie"] = "peepoHappyNuggie",
     ["peepoHug"] = "peepoHug",
     ["peepoShy"] = "peepoShy",
+    ["pepeW"] = "pepeW",
+    ["smoge"] = "smoge",
     ["susge"] = "susge",
     ["THYMINDE"] = "THYMINDE",
-    ["HappyPeepoFarmer"] = "HappyPeepoFarmer",
-    ["smoge"] = "smoge",
-    ["WideAYAYA"] = "WideAYAYA",
+
+    -- Large Emotes
+    ["ayayaKMSW"] = "ayayaKMSW",
     ["AYAYAW"] = "AYAYAW",
+
+    -- Wide Emotes
+    ["WideAYAYA"] = "WideAYAYA",
+    ["WideDistortedWeird"] = "WideDistortedWeird",
 }
 
 
 local solarisPack = {
+    ["ayayaKMS"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\ayayaKMS.tga:28:28",
+    ["AzorCool"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\AzorCool.tga:28:28",
     ["blusherBucket"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\blusherBucket.tga:28:28",
+    ["catDisgust"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\catDisgust.tga:28:28",
+    ["DistortedWeird"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\DistortedWeird.tga:28:28",
     ["happyAwesome"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\happyAwesome.tga:28:28",
+    ["HappyPeepoFarmer"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\HappyPeepoFarmer.tga:28:28",
+    ["NootLikeThis"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\NootLikeThis.tga:28:28",
     ["peepoFlower"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\peepoFlower.tga:28:28",
     ["peepoHappyNuggie"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\peepoHappyNuggie.tga:28:28",
     ["peepoHug"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\peepoHug.tga:28:28",
     ["peepoShy"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\peepoShy.tga:28:28",
+    ["pepeW"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\pepeW.tga:28:28",
+    ["smoge"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\smoge.tga:28:28",
     ["susge"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\susge.tga:28:28",
     ["THYMINDE"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\THYMINDE.tga:28:28",
-    ["smoge"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\smoge.tga:28:28",
-    ["WideAYAYA"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\AYAYAW.tga:28:128",
-    ["AYAYAW"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\AYAYAW.tga:LARGE",
+
+    -- Large Emotes
+    ["ayayaKMSW"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\large\\ayayaKMSW.tga:LARGE",
+    ["AYAYAW"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\large\\AYAYAW.tga:LARGE",
+    
+    -- Wide Emotes
+    ["WideAYAYA"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\wide\\WideAYAYA.tga:28:128",
+    ["WideDistortedWeird"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\wide\\WideDistortedWeird.tga:28:128",
 }
 
 
