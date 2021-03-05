f849e04e6b420a804d4e9c8977ae35038a415fad
Putro <laustsf@gmail.com>
2021-03-05 19:57:38 +0100

    Created TwitchEmotes_Solaris AddOn

---
+++ main.lua
@@ -0,0 +1,33 @@
+
+local solarisEmoticons = {
+    ["blusherBucket"] = "blusherBucket",
+    ["happyAwesome"] = "happyAwesome",
+    ["peepoFlower"] = "peepoFlower",
+    ["peepoHappyNuggie"] = "peepoHappyNuggie",
+    ["peepoHug"] = "peepoHug",
+    ["peepoShy"] = "peepoShy",
+    ["susge"] = "susge",
+    ["THYMINDE"] = "THYMINDE",
+    ["HappyPeepoFarmer"] = "HappyPeepoFarmer",
+    ["smoge"] = "smoge",
+    ["WideAYAYA"] = "WideAYAYA",
+    ["AYAYAW"] = "AYAYAW",
+}
+
+
+local solarisPack = {
+    ["blusherBucket"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\blusherBucket.tga:28:28",
+    ["happyAwesome"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\happyAwesome.tga:28:28",
+    ["peepoFlower"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\peepoFlower.tga:28:28",
+    ["peepoHappyNuggie"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\peepoHappyNuggie.tga:28:28",
+    ["peepoHug"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\peepoHug.tga:28:28",
+    ["peepoShy"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\peepoShy.tga:28:28",
+    ["susge"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\susge.tga:28:28",
+    ["THYMINDE"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\THYMINDE.tga:28:28",
+    ["smoge"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\smoge.tga:28:28",
+    ["WideAYAYA"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\AYAYAW.tga:28:128",
+    ["AYAYAW"] = "Interface\\AddOns\\TwitchEmotes_Solaris\\emotes\\AYAYAW.tga:LARGE",
+}
+
+
+Emoticons_RegisterPack("TwitchEmotes_Solaris", solarisEmoticons, solarisPack);
